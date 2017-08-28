c120ad7b9d7650302f04f670c4301edb20f3b18f
wangdong <wangdong@zhongan.io>
2017-08-28 10:59:01 +0800

    initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,29 @@
+// var LoopringToken = artifacts.require("./LoopringToken.sol")
+var TestERC20Token 	= artifacts.require("./TestERC20Token.sol")
+var MidTerm 		= artifacts.require("./LRCMidTermHoldingContract.sol")
+var LongTerm 		= artifacts.require("./LRCLongTermHoldingContract.sol")
+
+module.exports = function(deployer, network, accounts) {
+	console.log("network: " + network);
+	
+    if (network == "live") {
+    	var lrcAddress 		= "0xEF68e7C694F40c8202821eDF525dE3782458639f";
+    	var ownerAddress 	= "0x9167E8B2EeD2418Fa520C8C036d73ceE6b88aFE9";
+        deployer.deploy(MidTerm, lrcAddress, ownerAddress); 
+        deployer.deploy(LongTerm, lrcAddress);
+
+    } else {
+    	deployer.deploy(TestERC20Token)
+    	.then(function() {
+		  	return deployer.deploy(
+		  		MidTerm, 
+		  		TestERC20Token.address, 
+		  		accounts[0]);
+		})
+		.then(function(){
+			return deployer.deploy(
+				LongTerm, 
+				TestERC20Token.address);
+		});
+    }
+};
