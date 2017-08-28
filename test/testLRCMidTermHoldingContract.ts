44a8c50faa31da937c3a6c40f6d16f3a3c7011e0
Kongliang Zhong <zkl_1011@126.com>
2017-08-28 14:06:39 +0800

    add test for LRCMidTermHoldingContract

--- test/testLRCMidTermHoldingContract.ts
+++ test/testLRCMidTermHoldingContract.ts
@@ -1,49 +1,65 @@
 import * as BigNumber from 'bignumber.js';
+import Web3 = require('web3');
 import promisify = require('es6-promisify');
+const web3Instance: Web3 = web3;
 
 const LRCMidTermHoldingContract = artifacts.require('./LRCMidTermHoldingContract.sol')
 const TestERC20Token = artifacts.require('./TestERC20Token.sol')
 
 contract('LRCMidTermHoldingContract', (accounts: string[]) => {
 
-  const sender = accounts[4];
-  const ownerAddr = accounts[1];
-  const lrcAddr = accounts[2];
-  const midTermAddr = accounts[3];
+  const owner = accounts[0];
+  const sender = accounts[1];
 
   let testERC20Token: any;
   let midTerm: any;
+  let tokenAddr: string;
+  let contractAddr: string;
 
-  const getEthBalanceAsync = async (owner: string) => {
-    const balanceStr = await promisify(web3.eth.getBalance)(owner);
+  const getEthBalanceAsync = async (addr: string) => {
+    const balanceStr = await promisify(web3.eth.getBalance)(addr);
     const balance = new BigNumber(balanceStr);
     return balance;
   };
 
-  const getTokenBalanceAsync = async (owner: string) => {
-    const tokenBalanceStr = await testERC20Token.balanceOf(owner);
+  const getTokenBalanceAsync = async (addr: string) => {
+    const tokenBalanceStr = await testERC20Token.balanceOf(addr);
     const balance = new BigNumber(tokenBalanceStr);
     return balance;
   }
 
+  const sendTransaction = promisify(web3Instance.eth.sendTransaction);
+
   before(async () => {
     testERC20Token = await TestERC20Token.deployed();
     midTerm = await LRCMidTermHoldingContract.deployed();
-    testERC20Token.approve(sender, 1000);
-    console.log("midTerm.address:", midTerm.address);
+    await testERC20Token.setBalance(sender, web3.toWei(20000));
+    await testERC20Token.approve(sender, web3.toWei(1000));
+
+    const _tokenAddrInMidTerm = await midTerm.lrcTokenAddress.call();
+    const _ownerInMidTerm = await midTerm.owner.call();
+    console.log(testERC20Token.address, owner);
+    console.log(_tokenAddrInMidTerm, _ownerInMidTerm);
+
+    tokenAddr = testERC20Token.address;
+    contractAddr = midTerm.address;
   });
 
   describe('send eth to LRCMidTermHoldingContract', () => {
     it('should be able to transfer lrc token if approved.', async () => {
-      const midTermAddr = midTerm.address;
+      const closed = await midTerm.closed.call({from: owner});
+      const depositStartTime = await midTerm.depositStartTime.call();
+      const depositStopTime = await midTerm.depositStopTime.call();
+      const WITHDRAWAL_DELAY = await midTerm.WITHDRAWAL_DELAY.call();
+      const WITHDRAWAL_WINDOW = await midTerm.WITHDRAWAL_WINDOW.call();
 
-      const balanceStr = await web3.eth.getBalance(sender);
-      console.log("balanceStr:", balanceStr);
+      console.log("closed:", closed);
+      console.log("depositStartTime:", depositStartTime);
+      console.log("depositStopTime:", depositStopTime);
+      console.log("WITHDRAWAL_DELAY:", WITHDRAWAL_DELAY);
+      console.log("WITHDRAWAL_WINDOW:", WITHDRAWAL_WINDOW);
 
-      //let tx = await web3.eth.sendTransaction({from: sender, to: midTerm.address, value: web3.toWei(1), gas: 500000});
-
-      let tx = await web3.eth.sendTransaction({from: accounts[1], to: midTerm.address, value: web3.toWei(1), gas: 500000});
-      // console.log("tx:", tx);
+      await await sendTransaction({from: sender, to: contractAddr, value: 0, gas: 500000});
 
       const ethBalance = await getEthBalanceAsync(midTerm.address);
       console.log("ethBalance", ethBalance);
